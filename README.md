d9e29f03d57d0e1f6e795daa34c04272dd994c35
LeAnne B. <branchleannelmb@gmail.com>
2024-08-03 18:46:32 -0400

    adjusts for use on windows

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 A text editor in C, following this tutorial:
 https://viewsourcecode.org/snaptoken/kilo/01.setup.html
+
+Note: adapted the tutorial for Windows given that Mingw does not include the termios header file
--- kilo.c
+++ kilo.c
@@ -1,8 +1,22 @@
+#include <windows.h>
 #include <unistd.h>
 
+void enableRawMode() {
+    HANDLE hStdIn = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD dwMode = 0;
+
+    GetConsoleMode(hStdIn, &dwMode);
+
+    // Disable the line input, echo input, and process input modes
+    dwMode &= ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT | ENABLE_PROCESSED_INPUT);
+    SetConsoleMode(hStdIn, dwMode);
+}
+
 int main() {
+    enableRawMode();
     char c;
-    //We are asking read() to read 1 byte from the standard input into the variable c, and to keep doing it until there are no more bytes to read. read() returns the number of bytes that it read, and will return 0 when it reaches the end of a file.
+
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+
     return 0;
 }
