89a1206ea65aee7a661fd3300395a12accd2c2dc
LeAnne B. <branchleannelmb@gmail.com>
2024-08-03 22:14:20 -0400

    allows program to print ascii values for characters

--- kilo.c
+++ kilo.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -43,7 +44,13 @@ int main() {
     enableRawMode();
     char c;
 
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)){
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
 
     return 0;
 }
