c33a4ae0064538031084f2fe9313b6986d43a902
LeAnne B. <branchleannelmb@gmail.com>
2024-08-03 22:01:53 -0400

    makes compatible for both windows and unix

--- kilo.c
+++ kilo.c
@@ -1,7 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
 #include <windows.h>
+#include <io.h>
+#else
+#include <termios.h>
 #include <unistd.h>
+struct termios orig_termios;
+#endif
+
+void disableRawMode() {
+#ifdef _WIN32
+    // For Windows, this function could reset the console mode to its original state.
+    // Implementation can vary based on what modes need to be reset.
+    // For now, this is a placeholder as Windows does not provide an easy way to reset to original mode without storing it first.
+#else
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+#endif
+}
 
 void enableRawMode() {
+#ifdef _WIN32
     HANDLE hStdIn = GetStdHandle(STD_INPUT_HANDLE);
     DWORD dwMode = 0;
 
@@ -10,6 +30,13 @@ void enableRawMode() {
     // Disable the line input, echo input, and process input modes
     dwMode &= ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT | ENABLE_PROCESSED_INPUT);
     SetConsoleMode(hStdIn, dwMode);
+#else
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+#endif
 }
 
 int main() {
