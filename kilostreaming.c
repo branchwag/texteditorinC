f380d300a6920a003272ab56634e03541ecfaca2
branchwag <branchleannelmb@gmail.com>
2024-08-06 18:53:39 -0400

    adds error handling

--- kilostreaming.c
+++ kilostreaming.c
@@ -1,28 +1,17 @@
-/*** includes ***/
 #include <ctype.h>
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
-/*** data ***/
 struct termios orig_termios;
 
-/*** terminal ***/
-
-void die(const char *s) {
-  perror(s);
-  exit(1);
-}
-
 void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
-    die("tcsetattr");
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
 void enableRawMode() {
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+  tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
@@ -32,12 +21,9 @@ void enableRawMode() {
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
-  
   //apply 2 terminal
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetsttr");
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
-
-/*** init ***/
 
 int main() {
   enableRawMode();
@@ -45,7 +31,7 @@ int main() {
   //while there are bytes to read, read 1 byte from standard input into variable c untillllll we press 'q'
   while (1) {
     char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+    read(STDIN_FILENO, &c, 1);
     if(iscntrl(c)) {
 	printf("%d\r\n", c);
     } else {
