aad3e7eece22b9b5243d8a22ed7a2f581d2b52dd
branchwag <branchleannelmb@gmail.com>
2024-08-06 19:13:05 -0400

    clears screen and repositions cursor

--- kilo.c
+++ kilo.c
@@ -40,21 +40,42 @@ void enableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetsttr");
 }
 
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if(nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
+/*** output ***/
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/*** input ***/
+
+void editorProcessKeypress() {
+  char c = editorReadKey();
+
+  switch (c) {
+    case CTRL_KEY('q'):
+      exit(0);
+      break;
+  }
+}
+
 /*** init ***/
 
 int main() {
   enableRawMode();
  
-  //while there are bytes to read, read 1 byte from standard input into variable c untillllll we press 'q'
+  //while there are bytes to read, read 1 byte from standard input into variable c untillllll exit
   while (1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-    if(iscntrl(c)) {
-	printf("%d\r\n", c);
-    } else {
-	printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == CTRL_KEY('q')) break;
+    editorRefreshScreen();
+    editorProcessKeypress();
 }
   return 0;
 }
