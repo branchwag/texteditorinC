b33cf7013089e8b6d099e5d78a17e62eeaba5224
branchwag <branchleannelmb@gmail.com>
2024-08-06 21:18:04 -0400

    draws new screen with tilde chars

--- kilo.c
+++ kilo.c
@@ -15,6 +15,8 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
   perror(s);
   exit(1);
 }
@@ -50,8 +52,18 @@ char editorReadKey() {
 }
 
 /*** output ***/
+void editorDrawRows() {
+  int y;
+  for (y = 0; y < 24; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
+
 void editorRefreshScreen() {
   write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  editorDrawRows();
   write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
@@ -62,6 +74,8 @@ void editorProcessKeypress() {
 
   switch (c) {
     case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
   }
